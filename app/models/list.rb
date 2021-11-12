62e42d636f66be10d9157a77f37e5d25fa122334
Anju Mogi <anju.mogi@gmail.com>
2021-11-12 16:58:38 +0900

    only the styles....

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,4 +2,5 @@ class List < ApplicationRecord
   has_many :bookmarks
   has_many :movies, through: :bookmarks, dependent: :delete_all
   validates :name, presence: true, uniqueness: true
+  has_one_attached :photo
 end
